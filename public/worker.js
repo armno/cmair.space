b53e6c9a06866ee28e5ac41289bcc0f92ac5d605
Armno <monkeyarmno@gmail.com>
2017-03-29 11:24:13 +0700

    call self.skipwaiting after add cache

--- public/worker.js
+++ public/worker.js
@@ -9,35 +9,38 @@ const appShell = [
 ];
 
 self.addEventListener('install', (e) => {
-  console.log('[ServiceWorker] Install');
+	console.log('[ServiceWorker] Install');
 
-  // caching appshell
-  e.waitUntil(
+	// caching appshell
+	e.waitUntil(
 		caches.open(cacheName)
 			.then((cache) => {
-      console.log('[ServiceWorker] Caching app shell');
-      return cache.addAll(appShell);
-    })
-  );
+				console.log('[ServiceWorker] Caching app shell');
+				return cache.addAll(appShell);
+			})
+			.then(() => {
+				return self.skipWaiting();
+			})
+	);
 });
 
 self.addEventListener('activate', (e) => {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then((keyList) => {
-      return Promise.all(keyList.map((key) => {
-        if (key !== cacheName && key !== dataCacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
-  return self.clients.claim();
+	console.log('[ServiceWorker] Activate');
+	e.waitUntil(
+		caches.keys().then((keyList) => {
+			return Promise.all(keyList.map((key) => {
+				if (key !== cacheName && key !== dataCacheName) {
+					console.log('[ServiceWorker] Removing old cache', key);
+					return caches.delete(key);
+				}
+			}));
+		})
+	);
+	return self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
-  console.log('[ServiceWorker] Fetch', e.request.url);
+	console.log('[ServiceWorker] Fetch', e.request.url);
 	const dataUrl = '/api';
 	if (e.request.url.indexOf(dataUrl) > -1) {
 		console.log('[ServiceWorker] Returning cached API data');
@@ -53,8 +56,8 @@ self.addEventListener('fetch', (e) => {
 		console.log('[ServiceWorker] Requesting ...');
 		e.respondWith(
 			caches.match(e.request, {
-      	ignoreSearch: true
-    	}).then((response) => {
+				ignoreSearch: true
+			}).then((response) => {
 				return response || fetch(e.request);
 			})
 		);
