0555c82ce73cafc5c4326abb178fa9678a282663
Armno <monkeyarmno@gmail.com>
2018-04-22 10:14:46 +0700

    align text in select box to center, in chrome

--- public/worker.js
+++ public/worker.js
@@ -1,4 +1,4 @@
-const BUILD_NUMBER = '24';
+const BUILD_NUMBER = '26';
 const cacheName = `cm-aqi-${BUILD_NUMBER}`;
 const dataCacheName = `cm-aqi-data-${BUILD_NUMBER}`;
 const appShell = [
@@ -43,28 +43,28 @@ self.addEventListener('activate', e => {
 	return self.clients.claim();
 });
 
-// self.addEventListener('fetch', (e) => {
-// 	console.log('[ServiceWorker] Fetch', e.request.url);
-// 	const dataUrl = '/api';
-// 	if (e.request.url.indexOf(dataUrl) > -1) {
-// 		console.log('[ServiceWorker] Returning cached API data');
-// 		e.respondWith(
-// 			caches.open(dataCacheName).then((cache) => {
-// 				return fetch(e.request).then((response) => {
-// 					cache.put(e.request.url, response.clone());
-// 					return response;
-// 				});
-// 			})
-// 		);
-// 	} else {
-// 		console.log('[ServiceWorker] Requesting ...');
-// 		e.respondWith(
-// 			caches.match(e.request, {
-// 				ignoreSearch: true
-// 			}).then((response) => {
-// 				return response || fetch(e.request);
-// 			})
-// 		);
-// 	}
-//
-// });
+self.addEventListener('fetch', (e) => {
+	console.log('[ServiceWorker] Fetch', e.request.url);
+	const dataUrl = '/api';
+	if (e.request.url.indexOf(dataUrl) > -1) {
+		console.log('[ServiceWorker] Returning cached API data');
+		e.respondWith(
+			caches.open(dataCacheName).then((cache) => {
+				return fetch(e.request).then((response) => {
+					cache.put(e.request.url, response.clone());
+					return response;
+				});
+			})
+		);
+	} else {
+		console.log('[ServiceWorker] Requesting ...');
+		e.respondWith(
+			caches.match(e.request, {
+				ignoreSearch: true
+			}).then((response) => {
+				return response || fetch(e.request);
+			})
+		);
+	}
+
+});
