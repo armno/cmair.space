32a2f560bf2caa8b84ce526d9ff139991b0c41e4
Armno <monkeyarmno@gmail.com>
2020-05-04 14:13:18 +0700

    fetch aqi from the api

--- api/get-aqi.ts
+++ api/get-aqi.ts
@@ -1,5 +1,25 @@
 import { NowRequest, NowResponse } from '@now/node';
+import fetch from 'isomorphic-unfetch';
 
-export default (req: NowRequest, res: NowResponse) => {
-	res.json({ name: 'John', email: 'john@example.com' })
-}
+export default async (req: NowRequest, res: NowResponse) => {
+	const station = req.body.station || '@6817';
+
+	try {
+		const TOKEN = process.env.TOKEN;
+		const url = `https://api.waqi.info/feed/${station}/?token=${TOKEN}`;
+
+		const response = await fetch(url);
+
+		if (response.status >= 400) {
+			return res.status(400).json({
+				error: `cannot get aqi data`,
+			});
+		}
+
+		return res.status(200).json(response);
+	} catch (error) {
+		return res.status(500).json({
+			error: error.message || error.toString(),
+		});
+	}
+};
